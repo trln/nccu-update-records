087ad91c7fb646c008fb68afe5e9778013ca38b3
Gena Kazymova <genia@trln.org>
2019-03-13 14:53:58 -0400

    Fix path in cleanup and validate

--- cleanup_and_validate.rb
+++ cleanup_and_validate.rb
@@ -4,7 +4,6 @@ current_date = Time.now.strftime("%d-%m-%Y-%H")
 file = ARGV[0]
 
 puts "Processing #{file}..."
-path = "/home/ec2-user/data"
 
 system "ruby xml_cleanup.rb #{file} > #{path}/update-#{current_date}-better.xml"
 system "xmllint --recover #{path}/update-#{current_date}-better.xml > #{path}/update-#{current_date}-clean.xml"
