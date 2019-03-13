e694defa4b454d137ebf8fc15dd85b2546e7a28c
Gena Kazymova <genia@trln.org>
2019-03-13 15:03:56 -0400

    Fix path to the file

--- cleanup_and_validate.rb
+++ cleanup_and_validate.rb
@@ -5,7 +5,7 @@ file = ARGV[0]
 
 puts "Processing #{file}..."
 
-system "ruby xml_cleanup.rb #{file} > #{path}/update-#{current_date}-better.xml"
+system "ruby xml_cleanup.rb #{file} > /home/ec2-user/data/update/update-#{current_date}-better.xml"
 system "xmllint --recover #{path}/update-#{current_date}-better.xml > #{path}/update-#{current_date}-clean.xml"
 
 puts "Done!"
