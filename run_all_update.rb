be5034cdbe96b65d4486720fe83b2b7a3130e68b
Gena Kazymova <genia@trln.org>
2019-03-14 14:17:49 -0400

    Define current_date as a global variable

--- run_all_update.rb
+++ run_all_update.rb
@@ -1,19 +1,18 @@
 
 #run this file every hour
 path = "/home/ec2-user/data/update"
-
-current_date = Time.now.strftime("%d-%m-%Y-%H")
+$current_date = Time.now.strftime("%d-%m-%Y-%H")
 
 if File.exist?("#{path}/update-#{current_date}.xml")
-  system "ruby cleanup_and_validate.rb #{path}/update-#{current_date}.xml"
+  system "ruby cleanup_and_validate.rb #{path}/update-#{$current_date}.xml"
   puts "Clean up Done!"
 
   if File.exist?("#{path}/update-#{current_date}-clean.xml")
-    system "ruby run_mta.rb #{path}/update-#{current_date}-clean.xml"
+    system "ruby run_mta.rb #{path}/update-#{$current_date}-clean.xml"
     puts "MTA Done!"
     
     if File.exist?("#{path}/add-#{current_date}.json")
-      system "ruby ingest.rb #{path}/add-#{current_date}.json"
+      system "ruby ingest.rb #{path}/add-#{$current_date}.json"
       puts "Ingest Done!"
     end
   end    
--- run_mta.rb
+++ run_mta.rb
@@ -3,12 +3,12 @@ require 'logger'
 file = ARGV[0]
 
 puts "Processing #{file}..."
-output = system "../marc-to-argot/exe/mta create -t xml nccu #{file} #{path}/add-#{current_date}.json"
+output = system "../marc-to-argot/exe/mta create -t xml nccu #{file} #{path}/add-#{$current_date}.json"
 
 if output == false
-  log = Logger.new("/home/ec2-user/log/log-mta-#{current_date}.txt",  "monthly")
+  log = Logger.new("/home/ec2-user/log/log-mta-#{$current_date}.txt",  "monthly")
   subject = "NCCU: Marc-to-argot error"
-  message = "#{file} file produced a mta error"
+  message = "#{file} file produced a marc-to-argot error"
   log.error message
   send_email(subject, message)
 end  
