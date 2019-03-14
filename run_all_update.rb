db717e155eeb2c19709316de3819917666ee3e1b
Gena Kazymova <genia@trln.org>
2019-03-14 14:19:18 -0400

    Define current_date as a global variable

--- run_all_update.rb
+++ run_all_update.rb
@@ -3,15 +3,15 @@
 path = "/home/ec2-user/data/update"
 $current_date = Time.now.strftime("%d-%m-%Y-%H")
 
-if File.exist?("#{path}/update-#{current_date}.xml")
+if File.exist?("#{path}/update-#{$current_date}.xml")
   system "ruby cleanup_and_validate.rb #{path}/update-#{$current_date}.xml"
   puts "Clean up Done!"
 
-  if File.exist?("#{path}/update-#{current_date}-clean.xml")
+  if File.exist?("#{path}/update-#{$current_date}-clean.xml")
     system "ruby run_mta.rb #{path}/update-#{$current_date}-clean.xml"
     puts "MTA Done!"
     
-    if File.exist?("#{path}/add-#{current_date}.json")
+    if File.exist?("#{path}/add-#{$current_date}.json")
       system "ruby ingest.rb #{path}/add-#{$current_date}.json"
       puts "Ingest Done!"
     end
