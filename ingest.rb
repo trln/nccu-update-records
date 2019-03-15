e9bc489c38e1f3127709225ca4af104712aaa95d
Gena Kazymova <genia@trln.org>
2019-03-15 11:57:58 -0400

    Fix another bug

--- ingest.rb
+++ ingest.rb
@@ -7,12 +7,11 @@ current_date = Time.now.strftime("%d-%m-%Y-%H")
 file = ARGV[0]
 
 puts "Processing #{file}..."
-output = system "spofford ingest #{file}"
+output = system "spofford ingest #{file} 2>error.log"
 
 if output == false
   error = File.read("/home/ec2-user/nccu-update-records/error.log")
-  file_content = File.read("#{file}")
   subject = "NCCU: Ingest error"
-  message = "#{file} file produced an ingest error. \n \n Error: \n \n #{error}. \n \n File: \n \n #{file_content}."
+  message = "#{file} file produced an ingest error. \n \n Error: \n \n #{error}. \n \n"
   send_email(subject, message)
 end
--- run_mta.rb
+++ run_mta.rb
@@ -9,9 +9,8 @@ output = system "../marc-to-argot/exe/mta create -t xml nccu #{file} #{path}/add
 
 if output == false
   error = File.read("/home/ec2-user/nccu-update-records/error.log")
-  file_content = File.read("#{file}")
   subject = "NCCU: Marc-to-argot error"
-  message = "#{file} file produced a marc-to-argot error. \n \n Error: \n \n #{error}. \n \n File: \n \n #{file_content}."
+  message = "#{file} file produced a marc-to-argot error. \n \n Error: \n \n #{error}. \n \n"
   send_email(subject, message)
 end  
 
