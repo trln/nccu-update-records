f2f3553ec7438871b1e096102c6c9330231395d3
Gena Kazymova <genia@trln.org>
2019-03-15 11:53:32 -0400

    Fix bug

--- ingest.rb
+++ ingest.rb
@@ -10,7 +10,7 @@ puts "Processing #{file}..."
 output = system "spofford ingest #{file}"
 
 if output == false
-  error = File.read("/home/ec2-user/nccu-update-records.error.log")
+  error = File.read("/home/ec2-user/nccu-update-records/error.log")
   file_content = File.read("#{file}")
   subject = "NCCU: Ingest error"
   message = "#{file} file produced an ingest error. \n \n Error: \n \n #{error}. \n \n File: \n \n #{file_content}."
--- run_mta.rb
+++ run_mta.rb
@@ -8,7 +8,7 @@ puts "Processing #{file}..."
 output = system "../marc-to-argot/exe/mta create -t xml nccu #{file} #{path}/add-#{current_date}.json 2>error.log"
 
 if output == false
-  error = File.read("/home/ec2-user/nccu-update-records.error.log")
+  error = File.read("/home/ec2-user/nccu-update-records/error.log")
   file_content = File.read("#{file}")
   subject = "NCCU: Marc-to-argot error"
   message = "#{file} file produced a marc-to-argot error. \n \n Error: \n \n #{error}. \n \n File: \n \n #{file_content}."
