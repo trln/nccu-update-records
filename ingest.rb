5eee97d42ab2d4af3da64add9a21b1fb14597a30
Gena Kazymova <genia@trln.org>
2019-03-18 10:58:07 -0400

    Add different log files for mta and ingest

--- ingest.rb
+++ ingest.rb
@@ -4,10 +4,10 @@ require_relative 'amazon-ses'
 file = ARGV[0]
 
 puts "Processing #{file}..."
-output = system "spofford ingest #{file} 2>error.log"
+output = system "spofford ingest #{file} 2>ingest_error.log"
 
 if output == false
-  error = File.read("/home/ec2-user/nccu-update-records/error.log")
+  error = File.read("/home/ec2-user/nccu-update-records/ingest_error.log")
   subject = "NCCU: Ingest error"
   message = "#{file} file produced an ingest error. \n \n \n \n #{error}. \n \n"
   send_email(subject, message)
--- run_mta.rb
+++ run_mta.rb
@@ -5,12 +5,14 @@ require './variables.rb'
 file = ARGV[0]
 
 puts "Processing #{file}..."
-output = system "../marc-to-argot/exe/mta create -t xml nccu #{file} #{Variables::PATH}/add-#{Variables::CURRENT_DATE}.json 2>error.log"
+output = system "../marc-to-argot/exe/mta create -t xml nccu #{file} #{Variables::PATH}/add-#{Variables::CURRENT_DATE}.json 2>mta_error.log"
 
 if output == false
-  error = File.read("/home/ec2-user/nccu-update-records/error.log")
+  error = File.read("/home/ec2-user/nccu-update-records/mta_error.log")
   subject = "NCCU: Marc-to-argot error"
   message = "#{file} file produced a marc-to-argot error: \n \n \n \n #{error}. \n \n"
   send_email(subject, message)
-end  
+end
+
+puts "Done!" 
 
