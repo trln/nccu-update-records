0fc06fa6c7f47c832e8ada1d64fde6cf9c25930c
Gena Kazymova <genia@trln.org>
2019-03-19 12:15:07 -0400

    Update path to error log

--- ingest.rb
+++ ingest.rb
@@ -3,6 +3,6 @@ require './helper.rb'
 file = ARGV[0]
 
 puts "Processing #{file}..."
-output = system "spofford ingest #{file} 2>>/home/ec2-user/nccu-update-records/error.log"
+output = system "spofford ingest #{file} out>error.log"
 
 Helper.notify_about_errors(output, subject = "Ingest", file)
