77fb6c2df83bad041270c9deb9fb5ffbee9a0539
Gena Kazymova <genia@trln.org>
2019-03-19 12:55:33 -0400

    Add notify_about_errors to xml cleanup and validate

--- cleanup_and_validate.rb
+++ cleanup_and_validate.rb
@@ -3,7 +3,12 @@ file = ARGV[0]
 
 puts "Processing #{file}..."
 
-system "ruby xml_cleanup.rb #{file} > #{Helper::PATH}/update-#{Helper::CURRENT_DATE}-better.xml"
-system "xmllint --recover #{Helper::PATH}/update-#{Helper::CURRENT_DATE}-better.xml > #{Helper::PATH}/update-#{Helper::CURRENT_DATE}-clean.xml"
+output_1 = system "ruby xml_cleanup.rb #{file} > #{Helper::PATH}/update-#{Helper::CURRENT_DATE}-better.xml 2>error.log"
+
+Helper.notify_about_errors(output_1, subject = "XML Cleanup", file)
+
+output_2 = system "xmllint --recover #{Helper::PATH}/update-#{Helper::CURRENT_DATE}-better.xml > #{Helper::PATH}/update-#{Helper::CURRENT_DATE}-clean.xml 2>error.log" if output_1 == true
+
+Helper.notify_about_errors(output_1, subject = "XML Recover", file)
 
 puts "Done!"
