6f9cdb8d0a71068ff3b52ff8a008a1a774eda6c2
Gena Kazymova <genia@trln.org>
2019-03-19 11:49:39 -0400

    Fix in cleanup_and_validate.rb

--- cleanup_and_validate.rb
+++ cleanup_and_validate.rb
@@ -3,7 +3,7 @@ file = ARGV[0]
 
 puts "Processing #{file}..."
 
-system "ruby xml_cleanup.rb #{file} > #{Variables::PATH}/update-#{Variables::CURRENT_DATE}-better.xml"
-system "xmllint --recover #{Variables::PATH}/update-#{Variables::CURRENT_DATE}-better.xml > #{Variables::PATH}/update-#{Variables::CURRENT_DATE}-clean.xml"
+system "ruby xml_cleanup.rb #{file} > #{Helper::PATH}/update-#{Helper::CURRENT_DATE}-better.xml"
+system "xmllint --recover #{Helper::PATH}/update-#{Helper::CURRENT_DATE}-better.xml > #{Helper::PATH}/update-#{Helper::CURRENT_DATE}-clean.xml"
 
 puts "Done!"
