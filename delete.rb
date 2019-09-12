26bb4862e336bde9cc8e01314c4563c61039d0ca
Gena Kazymova <genia@trln.org>
2019-09-12 11:20:21 -0400

    Clean up the delete array

--- delete.rb
+++ delete.rb
@@ -4,6 +4,11 @@ require './helper.rb'
 delete_path = "#{Helper::PATH}/delete/delete-#{Helper::FILE_TO_DELETE}.json"
 
 if File.exist?(delete_path)
+  #clean up the array: remove a vertical bar
+  array = File.read(delete_path)
+  replace = array.gsub(/\|/,"");
+  File.open(delete_path, "w") {|file| file.puts replace}
+  
   output = system "spofford ingest #{delete_path} out>error.log"
   Helper.notify_about_errors(output, subject = "Delete documents error", delete_path)
 else
