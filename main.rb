d3b83b8538964d9f581256b9f8413e8a01a1fef7
Genia Kazymova <ek166@lib-m-3V3GTFM.local>
2019-09-20 10:29:53 -0400

    Change the path to the mrc and xml files

--- main.rb
+++ main.rb
@@ -2,7 +2,7 @@
 require './helper.rb'
 
 if File.exist?("#{Helper::PATH}/update/update-#{Helper::FILE_TO_UPDATE}.mrc")
-  system "yaz-marcdump -f MARC-8 -t UTF-8 -o marcxml update-#{Helper::FILE_TO_UPDATE}.mrc >update-#{Helper::FILE_TO_UPDATE}.xml"
+  system "yaz-marcdump -f MARC-8 -t UTF-8 -o marcxml #{Helper::PATH}/update/update-#{Helper::FILE_TO_UPDATE}.mrc >#{Helper::PATH}/update/update-#{Helper::FILE_TO_UPDATE}.xml"
   puts "Converted to MARCXML."
 
   if File.exist?("#{Helper::PATH}/update/update-#{Helper::FILE_TO_UPDATE}.xml")
