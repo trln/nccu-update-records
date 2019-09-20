3c4918444fba03e87b55cff1e48609f774341769
Genia Kazymova <ek166@lib-m-3V3GTFM.local>
2019-09-20 10:51:25 -0400

    Remove xml temporarily

--- main.rb
+++ main.rb
@@ -2,16 +2,16 @@
 require './helper.rb'
 
 if File.exist?("#{Helper::PATH}/update/update-#{Helper::FILE_TO_UPDATE}.mrc")
-  system "yaz-marcdump -f MARC-8 -t UTF-8 -o marcxml #{Helper::PATH}/update/update-#{Helper::FILE_TO_UPDATE}.mrc >#{Helper::PATH}/update/update-#{Helper::FILE_TO_UPDATE}.xml"
-  puts "Converted to MARCXML."
+  #system "yaz-marcdump -f MARC-8 -t UTF-8 -o marcxml #{Helper::PATH}/update/update-#{Helper::FILE_TO_UPDATE}.mrc >#{Helper::PATH}/update/update-#{Helper::FILE_TO_UPDATE}.xml"
+  #puts "Converted to MARCXML."
 
-  if File.exist?("#{Helper::PATH}/update/update-#{Helper::FILE_TO_UPDATE}.xml")
-    system "ruby cleanup_and_validate.rb #{Helper::PATH}/update/update-#{Helper::FILE_TO_UPDATE}.xml"
-    puts "Clean up Done!"
+  #if File.exist?("#{Helper::PATH}/update/update-#{Helper::FILE_TO_UPDATE}.xml")
+    #system "ruby cleanup_and_validate.rb #{Helper::PATH}/update/update-#{Helper::FILE_TO_UPDATE}.xml"
+    #puts "Clean up Done!"
 
-    if File.exist?("#{Helper::PATH}/update/update-#{Helper::FILE_TO_UPDATE}-clean.xml")
-      system "ruby marc_to_argot.rb #{Helper::PATH}/update/update-#{Helper::FILE_TO_UPDATE}-clean.xml"
-      #system "ruby marc_to_argot.rb #{Helper::PATH}/update/update-#{Helper::FILE_TO_UPDATE}.mrc"
+    #if File.exist?("#{Helper::PATH}/update/update-#{Helper::FILE_TO_UPDATE}-clean.xml")
+      #system "ruby marc_to_argot.rb #{Helper::PATH}/update/update-#{Helper::FILE_TO_UPDATE}-clean.xml"
+      system "ruby marc_to_argot.rb #{Helper::PATH}/update/update-#{Helper::FILE_TO_UPDATE}.mrc"
 
       puts "MTA Done!"
     
@@ -19,8 +19,8 @@ if File.exist?("#{Helper::PATH}/update/update-#{Helper::FILE_TO_UPDATE}.mrc")
         system "ruby ingest.rb #{Helper::PATH}/update/add-#{Helper::FILE_TO_UPDATE}.json"
         puts "Ingest Done!"
       end
-    end
-  end  
+    #end
+  #end  
 else
   puts "Nothing to update."      
 end
