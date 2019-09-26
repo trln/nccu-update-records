0f8998c52c9ef8ec78ece4a1b86bbace72535219
Genia Kazymova <ek166@lib-m-3V3GTFM.local>
2019-09-26 14:51:22 -0400

    Update mta error notification

--- main.rb
+++ main.rb
@@ -1,22 +1,22 @@
 #run once a day
 require_relative "./helper.rb"
 
-if File.exist?("#{Helper::PATH}/update/update-#{Helper::FILE_TO_UPDATE}.mrc")
-  #system "yaz-marcdump -f MARC-8 -t UTF-8 -o marcxml #{Helper::PATH}/update/update-#{Helper::FILE_TO_UPDATE}.mrc >#{Helper::PATH}/update/update-#{Helper::FILE_TO_UPDATE}.xml"
+if File.exist?("#{Helper::PATH}/data/update/update-#{Helper::FILE_TO_UPDATE}.mrc")
+  #system "yaz-marcdump -f MARC-8 -t UTF-8 -o marcxml #{Helper::PATH}/data/update/update-#{Helper::FILE_TO_UPDATE}.mrc >#{Helper::PATH}/update/update-#{Helper::FILE_TO_UPDATE}.xml"
   #puts "Converted to MARCXML."
 
-  #if File.exist?("#{Helper::PATH}/update/update-#{Helper::FILE_TO_UPDATE}.xml")
-    #system "ruby cleanup_and_validate.rb #{Helper::PATH}/update/update-#{Helper::FILE_TO_UPDATE}.xml"
+  #if File.exist?("#{Helper::PATH}/data/update/update-#{Helper::FILE_TO_UPDATE}.xml")
+    #system "ruby cleanup_and_validate.rb #{Helper::PATH}/data/update/update-#{Helper::FILE_TO_UPDATE}.xml"
     #puts "Clean up Done!"
 
-    #if File.exist?("#{Helper::PATH}/update/update-#{Helper::FILE_TO_UPDATE}-clean.xml")
-      #system "ruby marc_to_argot.rb #{Helper::PATH}/update/update-#{Helper::FILE_TO_UPDATE}-clean.xml"
-      system "ruby nccu-update-records/marc_to_argot.rb #{Helper::PATH}/update/update-#{Helper::FILE_TO_UPDATE}.mrc"
+    #if File.exist?("#{Helper::PATH}/data/update/update-#{Helper::FILE_TO_UPDATE}-clean.xml")
+      #system "ruby marc_to_argot.rb #{Helper::PATH}/data/update/update-#{Helper::FILE_TO_UPDATE}-clean.xml"
+      system "ruby nccu-update-records/marc_to_argot.rb #{Helper::PATH}/data/update/update-#{Helper::FILE_TO_UPDATE}.mrc"
 
       puts "MTA Done!"
     
-      if File.exist?("#{Helper::PATH}/update/add-#{Helper::FILE_TO_UPDATE}.json")
-        system "ruby nccu-update-records/ingest.rb #{Helper::PATH}/update/add-#{Helper::FILE_TO_UPDATE}.json"
+      if File.exist?("#{Helper::PATH}/data/update/add-#{Helper::FILE_TO_UPDATE}.json")
+        system "ruby nccu-update-records/ingest.rb #{Helper::PATH}/data/update/add-#{Helper::FILE_TO_UPDATE}.json"
         puts "Ingest Done!"
       end
     #end
