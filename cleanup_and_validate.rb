16e2cdff7e86b18d2ac781d74d5a53f94d52cd09
Genia Kazymova <ek166@lib-m-3V3GTFM.local>
2019-09-27 12:02:23 -0400

    Remove some comments

--- cleanup_and_validate.rb
+++ cleanup_and_validate.rb
@@ -4,11 +4,11 @@ file = ARGV[0]
 
 puts "Processing #{file}..."
 
-cleanup_output = system "ruby xml_cleanup.rb #{file} > #{Helper::PATH}/update/update-#{Helper::FILE_TO_UPDATE}-better.xml 2>error.log"
+cleanup_output = system "ruby xml_cleanup.rb #{file} > #{Helper::PATH}/data/update/update-#{Helper::FILE_TO_UPDATE}-better.xml 2>error.log"
 Helper.notify_about_errors(cleanup_output, subject = "XML Cleanup", file)
 
 if cleanup_output == true
-  recovery_output = system "xmllint --recover #{Helper::PATH}/update/update-#{Helper::FILE_TO_UPDATE}-better.xml > #{Helper::PATH}/update/update-#{Helper::FILE_TO_UPDATE}-clean.xml 2>error.log"
+  recovery_output = system "xmllint --recover #{Helper::PATH}/data/update/update-#{Helper::FILE_TO_UPDATE}-better.xml > #{Helper::PATH}/data/update/update-#{Helper::FILE_TO_UPDATE}-clean.xml 2>error.log"
   Helper.notify_about_errors(recovery_output, subject = "XML Recover", file)
 end  
 
--- delete.rb
+++ delete.rb
@@ -9,8 +9,9 @@ if File.exist?(delete_path)
   replace = array.gsub(/\|/,"");
   File.open(delete_path, "w") {|file| file.puts replace}
   
-  output = system "spofford ingest #{delete_path} out>error.log"
   puts "Processing file #{delete_path}"
+  output = system "spofford ingest #{delete_path} out>#{Helper::PATH}/error.log"
+  
   Helper.notify_about_errors(output, subject = "Delete documents error", delete_path)
 else
   puts "Nothing to delete."  
