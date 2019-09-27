70d2f4ae1bf3ed2f8685b23a6f424b4e7599afad
Genia Kazymova <ek166@lib-m-3V3GTFM.local>
2019-09-27 12:35:52 -0400

    Update path

--- ingest.rb
+++ ingest.rb
@@ -2,7 +2,8 @@ require_relative "./helper.rb"
 
 file = ARGV[0]
 
-puts "Processing #{file}..."
+#puts "Processing #{file}..."
+system "cd nccu-update-records"
 system "spofford ingest #{file}"
 
 #Helper.notify_about_errors(output, subject = "Ingest", file)
--- marc_to_argot.rb
+++ marc_to_argot.rb
@@ -2,8 +2,8 @@ require_relative "./helper.rb"
 
 file = ARGV[0]
 
-puts "Processing #{file}..."
-system "../marc-to-argot/exe/mta create -t binary nccu #{file} #{Helper::PATH}/update/add-#{Helper::FILE_TO_UPDATE}.json"
+#puts "Processing #{file}..."
+system "../marc-to-argot/exe/mta create -t binary nccu #{file} #{Helper::PATH}/data/update/add-#{Helper::FILE_TO_UPDATE}.json"
 
 #Helper.notify_about_errors(output, subject = "Marc-to-argot", file)
 
