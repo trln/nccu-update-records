304d58604c5f84c8500c4fd25e913059947f125f
Genia Kazymova <ek166@lib-m-3V3GTFM.local>
2019-09-27 14:29:59 -0400

    Update delete

--- ingest.rb
+++ ingest.rb
@@ -4,8 +4,9 @@ file = ARGV[0]
 
 puts "Processing #{file}..."
 
-system "spofford ingest #{file}"
+output = system "spofford ingest #{file}"
 
+puts output
 puts "Ingest done!"
 
 #Helper.notify_about_errors(output, subject = "Ingest", file)
