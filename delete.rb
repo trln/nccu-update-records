34c0acd4f2716fe49e3310df4c2cae344994f944
Genia Kazymova <ek166@lib-m-3V3GTFM.local>
2019-10-02 13:22:10 -0400

    Notify admin about mta and spofford ingest output

--- delete.rb
+++ delete.rb
@@ -10,9 +10,10 @@ if File.exist?(delete_path)
   File.open(delete_path, "w") {|file| file.puts replace}
   
   puts "Processing file #{delete_path}"
-  output = system "spofford ingest #{delete_path} out>#{Helper::PATH}/error.log"
+  output = "curl -v -H'Content-Type: application/json' -H'X-User-Email: genia@trln.org' -H'X-User-Token: sdjSYp8tQ8x-ZNBSHjbj' --data-binary @/home/ec2-user/data/delete/delete-10-01-2019.json https://ingest.discovery.trln.org/ingest/nccu"
+  #output = system "spofford ingest #{delete_path} out>#{Helper::PATH}/error.log"
   
-  Helper.notify_about_errors(output, subject = "Delete documents error", delete_path)
+  #Helper.notify_about_errors(output, subject = "Delete documents error", delete_path)
 else
   puts "Nothing to delete."  
 end
--- rotate_files.rb
+++ rotate_files.rb
@@ -3,3 +3,5 @@
 system 'find /home/ec2-user/data/update/* -mtime +14 -exec rm {} \;'
 system 'find /home/ec2-user/data/delete/* -mtime +14 -exec rm {} \;'
 system 'find /home/ec2-user/data/packages/* -mtime +14 -exec rm {} \;'
+
+system 'find /home/ec2-user/cronlogs/* -mtime +14 -exec rm {} \;' 
