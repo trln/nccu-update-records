f6f5ebfade93caaab926610f4c17075f3c2101b7
Genia Kazymova <ek166@lib-m-3V3GTFM.local>
2019-10-02 15:00:44 -0400

    Refactor notify.rb

--- notify.rb
+++ notify.rb
@@ -1,19 +1,21 @@
 require_relative "./helper.rb"
 
-update_log_path = "#{Helper::PATH}/cronlogs/updates_#{Helper::FILE_TO_UPDATE}.log"
-delete_log_path = "#{Helper::PATH}/cronlogs/deletes_#{Helper::FILE_TO_DELETE}.log"
+update_log_path = "#{Helper::PATH}/cronlogs/update_#{Helper::FILE_TO_UPDATE}.log"
+delete_log_path = "#{Helper::PATH}/cronlogs/delete_#{Helper::FILE_TO_DELETE}.log"
 
 file_to_update = "#{Helper::PATH}/data/update/add-#{Helper::FILE_TO_UPDATE}.json"
 file_to_delete = "#{Helper::PATH}/data/delete/delete-#{Helper::FILE_TO_DELETE}.json"
 
-if File.exist?(update_log_path)
-	puts "File exists"
-	contents = File.read(update_log_path)
-	puts contents
-	Helper.notify_about_errors(contents, subject = "updates", file_to_update)
+processes = ["update", "delete"]
+
+for i in processes do
+  message = create_message(i)
+  Helper.send_email(subject = "NCCU #{i} log", message)
 end
 
-if File.exist?(delete_log_path)
-	contents = File.read(delete_log_path)
-	Helper.notify_about_errors(contents, subject = "deletes", file_to_delete)
+def create_message(process)
+	if File.exists?("#{process}_log_path")
+	  contents = File.read("#{process}_log_path")
+	  message = "Below is the NCCU #{process} log: \n\n #{contents}. \n\n If you see any errors in this log, you need to log in to aws via ssh and debug the errors."
+    end
 end
