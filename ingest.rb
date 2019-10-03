fd2f6b7c8e8bbfac2f7ba5967e1b7aaa7bdda659
Genia Kazymova <ek166@lib-m-3V3GTFM.local>
2019-10-03 10:42:31 -0400

    Clean up

--- ingest.rb
+++ ingest.rb
@@ -2,7 +2,8 @@ require_relative "./helper.rb"
 
 file = ARGV[0]
 
-puts "Processing #{file}..."
+puts "Ingest:\n\n"
+puts "Processing #{file}...\n\n"
 
 #output = system "spofford ingest --config=/home/ec2-user/.spofford-client.yml #{file}" #spofford ingest didn't work via cron
 system "curl -v -H'Content-Type: application/json' -H'X-User-Email: genia@trln.org' -H'X-User-Token: sdjSYp8tQ8x-ZNBSHjbj' --data-binary @#{file} https://ingest.discovery.trln.org/ingest/nccu"
