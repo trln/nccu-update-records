9286a230ca304f9ebdd002612b8a95d0c2c329bf
Genia Kazymova <ek166@lib-m-3V3GTFM.local>
2019-10-09 15:27:33 -0400

    Fix curl delete command

--- delete.rb
+++ delete.rb
@@ -10,7 +10,7 @@ if File.exist?(delete_path)
   File.open(delete_path, "w") {|file| file.puts replace}
   
   puts "Processing file #{delete_path}"
-  output = "curl -v -H'Content-Type: application/json' -H'X-User-Email: genia@trln.org' -H'X-User-Token: sdjSYp8tQ8x-ZNBSHjbj' --data-binary @#{delete_path} https://ingest.discovery.trln.org/ingest/nccu"
+  system "curl -v -H'Content-Type: application/json' -H'X-User-Email: genia@trln.org' -H'X-User-Token: sdjSYp8tQ8x-ZNBSHjbj' --data-binary @#{delete_path} https://ingest.discovery.trln.org/ingest/nccu"
   #output = system "spofford ingest #{delete_path}" #spofford ingest didn't work via cron
 else
   puts "Nothing to delete."  
