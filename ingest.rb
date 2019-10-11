ae6148e0a47847455f625112efd3becac67dc90c
Genia Kazymova <ek166@lib-m-3V3GTFM.local>
2019-10-11 15:50:26 -0400

    Fix ingest

--- ingest.rb
+++ ingest.rb
@@ -5,5 +5,8 @@ file = ARGV[0]
 puts "Ingest:\n\n"
 puts "Processing #{file}...\n\n"
 
-#output = system "spofford ingest --config=/home/ec2-user/.spofford-client.yml #{file}" #spofford ingest didn't work via cron
-system "curl -v -H'Content-Type: application/json' -H'X-User-Email: genia@trln.org' -H'X-User-Token: sdjSYp8tQ8x-ZNBSHjbj' --data-binary @#{file} https://ingest.discovery.trln.org/ingest/nccu"
+#ENV['PATH'] = "#{ENV['PATH']}:/home/ec2-user/bin"
+#puts `which spofford`
+
+system "spofford ingest --config=/home/ec2-user/.spofford-client.yml #{file}"
+#system "curl -v -H'Content-Type: application/json' -H'X-User-Email: genia@trln.org' -H'X-User-Token: sdjSYp8tQ8x-ZNBSHjbj' --data-binary @#{file} https://ingest.discovery.trln.org/ingest/nccu"
