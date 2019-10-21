2f0bd89a8f55ca83dfef9163c987fafb55830efd
Genia Kazymova <ek166@lib-m-3V3GTFM.local>
2019-10-21 14:26:46 -0400

    Fix the attachments array

--- marc_to_argot.rb
+++ marc_to_argot.rb
@@ -3,7 +3,8 @@ require_relative "./helper.rb"
 current_file = ARGV[0]
 update = ARGV[1]
 
-puts "Marc-to-argot: #{update} change(s)\n\n"
+puts "============================================"
+puts "\n\nMarc-to-argot: #{update} change(s)\n\n"
 puts "Processing #{current_file}...\n\n"
 
 system "#{Helper::PATH}/marc-to-argot/exe/mta create -t binary nccu #{current_file} #{Helper::PATH}/data/update/add-#{update}-#{Helper::FILE_TO_UPDATE}.json"
