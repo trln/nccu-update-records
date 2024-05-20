f5723bf4722c583f2af948bc8389afbdb000af9d
EC2 Default User <ec2-user@ip-10-0-1-200.ec2.internal>
2024-05-20 20:00:29 +0000

    Fix the deletes for reshare

--- reshare/deletes.rb
+++ reshare/deletes.rb
@@ -1,13 +1,19 @@
 require_relative ".././helper.rb"
 
-FILE = "#{Helper::PATH}/data/delete/deletes-#{Helper::TIMESTAMP}.json"
+FILE = "#{Helper::PATH}/data/delete/delete-#{Helper::TIMESTAMP}.json"
+DELETE_FILE_NAME_RESHARE = "delete-#{Helper::TIMESTAMP}.del"
+DELETE_PATH_RESHARE = "#{Helper::PATH}/data/delete/delete-#{Helper::TIMESTAMP}.del"
 
-DELETE_FILE_NAME_RESHARE = "deletes-#{Helper::TIMESTAMP}.del"
-DELETE_PATH_RESHARE = "#{Helper::PATH}/data/delete/deletes-#{Helper::TIMESTAMP}.del"
 
 if File.exist?(FILE)
-  replace = arrey.gsub("NCCU","").gsub("[","").gsub("]","").gsub(",","\n").gsub('"',"");
-  File.open(DELETE_PATH_RESHARE, "w") {|file| file.puts replace} 
+  file_content = File.read(FILE)
+  replace = file_content.gsub("NCCU", "")
+                      .gsub("[", "")
+                      .gsub("]", "")
+                      .gsub(",", "\n")
+                      .gsub('"', "")
+  File.open(DELETE_PATH_RESHARE, "w") { |file| file.puts replace }
+ 
   if File.exist?(DELETE_PATH_RESHARE)
     system "curl -F 'upload[name]=#{DELETE_FILE_NAME_RESHARE}' -F 'upload[files][]=@/#{DELETE_PATH_RESHARE};type=text/plain' -H 'Authorization: Bearer #{ENV['ACCESS_TOKEN']}' https://pod.stanford.edu/organizations/#{ENV['ORG_CODE']}/uploads?stream=#{ENV['STREAM_ID']}"
   end
